4a2c42aac3dfe59f0d7dc3b89c4d2ac65be5d088
Gopala <guptaraja727@gmail.com>
2022-05-15 10:00:12 +0530

    secont commit

--- Api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-
-const app = express()
-
-app.listen(2345,()=>{
-    console.log("listening Port from 2345")
-})
---
+++ Api/src/server.js
@@ -0,0 +1,5 @@
+const app = require("./index")
+
+app.listen(2345,()=>{
+    console.log("listening on port 2345")
+})
