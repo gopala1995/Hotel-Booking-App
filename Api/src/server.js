d45ce743a1ca058ade2a32f14466655324265114
Gopala <guptaraja727@gmail.com>
2022-05-19 11:55:18 +0530

    second

--- Api/src/server.js
+++ Api/src/server.js
@@ -1,5 +1,16 @@
-const app = require("./index")
+const app = require("./index");
+const connect = require("../src/config/db");
 
-app.listen(2345,()=>{
-    console.log("listening on port 2345")
+
+app.get("/", async(req,res)=>{
+   res.send("Hey i'm responce")
 })
+
+app.listen(2345, async () => {
+  try {
+    await connect();
+    console.log("listening on port 2345");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
